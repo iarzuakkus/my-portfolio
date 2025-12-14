a9081fe9ba1acaa79202074fff1054821f529ac2
Arzu Akkus <ilaydaarzuakkus@gmail.com>
2025-12-14 15:40:14 +0300

    hero tasarim iyilestirmeleri yapildi

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,22 +2,29 @@
 
 import "./hero.css";
 import heroImage from "../../assets/images/hero-profile.png";
+import leftWave1 from "../../assets/icons/leftwave1.svg";
+import leftWave2 from "../../assets/icons/leftwave2.svg";
+import rightWave from "../../assets/icons/rightwave.svg";
+import shine from "../../assets/icons/shine.svg";
 
 export default function Hero() {
   return (
     <section className="hero-section">
-      {/* Container yerine hero-inner sınıfını ekledik */}
+      {/* Dekoratif Öğeler */}
+      <img src={shine} className="decor-shine shine-1" alt="" />
+      <img src={shine} className="decor-shine shine-2" alt="" />
+      
+      <img src={leftWave1} className="decor-wave wave-1" alt="" />
+      <img src={leftWave2} className="decor-wave wave-2" alt="" />
+      <img src={rightWave} className="decor-wave wave-3" alt="" />
+
       <div className="hero-inner"> 
-        
         {/* LEFT TEXT */}
         <div className="hero-text">
-          {/* 1. SATIR */}
           <div className="hero-row">
             <span className="hero-line yellow">Merhaba</span>
             <span className="hero-line purple">Ben Arzu</span>
           </div>
-
-          {/* 2. SATIR */}
           <div className="hero-line green hero-wide">
             <span>Bilgisayar Mühendisiyim</span>
           </div>
@@ -27,7 +34,6 @@ export default function Hero() {
         <div className="hero-image-wrapper">
           <img src={heroImage} alt="İ. Arzu Akkuş" />
         </div>
-
       </div>
     </section>
   );
